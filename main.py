9562cf8e0ce07ca7b4c5e1cbb5170c0cf1e23e61
ZonaSur26 <iaas.zonasur@gmail.com>
2026-07-15 14:26:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from ventanas import Unidad_Notificante, Identificacion_paciente, Hospitalizacion # Importamos el nuevo archivo
+from ventanas import Unidad_Notificante, Identificacion_paciente, Hospitalizacion, Antecedentes
 
 st.set_page_config(page_title="EpidemioManager", layout="wide")
 
@@ -7,23 +7,29 @@ st.set_page_config(page_title="EpidemioManager", layout="wide")
 if 'pagina_actual' not in st.session_state:
     st.session_state.pagina_actual = "Unidad Notificante"
 
+# Diccionario de navegación actualizado
 paginas = {
     "Unidad Notificante": Unidad_Notificante,
     "Identificación Paciente": Identificacion_paciente,
-    "Datos de hospitalización": Hospitalizacion # Añadido aquí
+    "Datos de hospitalización": Hospitalizacion,
+    "Antecedentes": Antecedentes
 }
 
 def main():
     st.sidebar.title("Menú Principal")
     
     opciones = list(paginas.keys())
+    # Usamos 'index' para controlar qué opción está seleccionada según el session_state
     seleccion = st.sidebar.radio(
         "Navegación", 
         opciones, 
         index=opciones.index(st.session_state.pagina_actual)
     )
     
-    st.session_state.pagina_actual = seleccion
+    # Actualizamos el estado si el usuario cambia manualmente en el menú
+    if st.session_state.pagina_actual != seleccion:
+        st.session_state.pagina_actual = seleccion
+        st.rerun()
     
     # Invocamos la función 'render' del módulo seleccionado
     pagina = paginas[seleccion]
