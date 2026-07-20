726e336105c9ece99e91b9987a3784087f86ddd3
ZonaSur26 <iaas.zonasur@gmail.com>
2026-07-20 07:59:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from ventanas.Hospitalizacion import render as render_hosp
 from ventanas.Antecedentes import render as render_ante
 from ventanas.IAAS import render as render_iaas
 from ventanas.Microbiologia import render as render_micro
-from ventanas.Polimicrobiana import render as render_poli # <--- NUEVA IMPORTACIÓN
+from ventanas.Polimicrobiana import render as render_poli
+from ventanas.Tratamiento import render as render_tratamiento # <--- NUEVA IMPORTACIÓN
 
 st.set_page_config(page_title="EpidemioManager", layout="wide")
 
@@ -19,24 +20,8 @@ paginas = {
     "Antecedentes Personales": render_ante,
     "IAAS y Factores de Riesgo": render_iaas,
     "Diagnóstico Microbiológico": render_micro,
-    "Infección Polimicrobiana": render_poli, # <--- NUEVA PESTAÑA
+    "Infección Polimicrobiana": render_poli,
+    "Tratamiento de IAAS": render_tratamiento, # <--- NUEVA PESTAÑA
 }
 
-def main():
-    st.sidebar.title("Menú Principal")
-    opciones = list(paginas.keys())
-    
-    seleccion = st.sidebar.radio(
-        "Navegación", 
-        opciones, 
-        index=opciones.index(st.session_state.pagina_actual) if st.session_state.pagina_actual in opciones else 0
-    )
-    
-    if st.session_state.pagina_actual != seleccion:
-        st.session_state.pagina_actual = seleccion
-        st.rerun()
-    
-    paginas[seleccion]()
-
-if __name__ == "__main__":
-    main()
+# ... (resto de la función main)
