e84215acbdc52a6829c8d3109d447ab3de07f64c
ZonaSur26 <iaas.zonasur@gmail.com>
2026-07-20 09:02:57 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+ORDEN = [
+    "Unidad Notificante", 
+    "Identificación Paciente", 
+    "Datos de hospitalización",
+    "Antecedentes Personales", 
+    "IAAS y Factores de Riesgo", 
+    "Diagnóstico Microbiológico",
+    "Infección Polimicrobiana", 
+    "Tratamiento de IAAS", 
+    "Detección y Notificación"
+]
