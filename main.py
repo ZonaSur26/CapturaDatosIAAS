b18815131797ba0387b128e80ad6309a6049335b
ZonaSur26 <iaas.zonasur@gmail.com>
2026-07-20 08:35:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import locale
 from ventanas.Unidad_Notificante import render as render_unidad
 from ventanas.Identificacion_paciente import render as render_paciente
 from ventanas.Hospitalizacion import render as render_hosp
@@ -7,7 +8,16 @@ from ventanas.IAAS import render as render_iaas
 from ventanas.Microbiologia import render as render_micro
 from ventanas.Polimicrobiana import render as render_poli
 from ventanas.Tratamiento import render as render_tratamiento
-from ventanas.Deteccion import render as render_deteccion # <--- NUEVA IMPORTACIÓN
+from ventanas.Deteccion import render as render_deteccion
+
+# Configuración de idioma para fechas
+try:
+    locale.setlocale(locale.LC_ALL, 'es_MX.UTF-8')
+except locale.Error:
+    try:
+        locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
+    except:
+        pass
 
 st.set_page_config(page_title="EpidemioManager", layout="wide")
 
@@ -23,7 +33,7 @@ paginas = {
     "Diagnóstico Microbiológico": render_micro,
     "Infección Polimicrobiana": render_poli,
     "Tratamiento de IAAS": render_tratamiento,
-    "Detección y Notificación": render_deteccion, # <--- NUEVA PESTAÑA
+    "Detección y Notificación": render_deteccion,
 }
 
 def main():
