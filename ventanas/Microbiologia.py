5eeadb188ac35d6e0eed3622d9c6a011b0b09006
ZonaSur26 <iaas.zonasur@gmail.com>
2026-07-19 18:25:48 -0600

    Update Microbiologia.py

--- ventanas/Microbiologia.py
+++ ventanas/Microbiologia.py
@@ -196,9 +196,7 @@ def render():
                     st.markdown('</div>', unsafe_allow_html=True)
 
         st.write("---")
-            realizo_comp = st.radio("¿SE REALIZÓ PRUEBA COMPLEMENTARIA PARA LA IDENTIFICACIÓN DE RESISTENCIA ANTIMICROBIANA?", ["No", "Sí"], index=None, horizontal=True)
-            if realizo_comp == "Sí":
-                st.text_input("Especifique la prueba complementaria:")
+            st.radio("¿SE REALIZÓ PRUEBA COMPLEMENTARIA PARA LA IDENTIFICACIÓN DE RESISTENCIA ANTIMICROBIANA?", ["No", "Sí"], index=None, horizontal=True)
 
         if st.button("Guardar Microbiología"):
             st.success("Datos guardados correctamente.")
